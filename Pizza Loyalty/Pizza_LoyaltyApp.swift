ce9ea183145802064be4e62d171785dfe55a1df1
Ozgun Aksoy <zgnaksoy@gmail.com>
2024-03-22 15:11:47 -0700

    Initial Commit

---
+++ Pizza Loyalty/Pizza_LoyaltyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_LoyaltyApp.swift
+//  Pizza Loyalty
+//
+//  Created by özgün aksoy on 2024-03-22.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_LoyaltyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
